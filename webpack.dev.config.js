648a23dfb7b04d4edcef374eccd5c9b0909ebde7
Geo Galagaran <geo.galagaran@gmail.com>
2018-06-16 10:22:26 +0800

    build(webpack-dev): Change source map to cheap-eval

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -5,7 +5,7 @@ const pubPath = path.resolve(__dirname, 'public')
 const WebpackHMR = new webpack.HotModuleReplacementPlugin()
 
 module.exports = {
-  devtool: 'inline-source-map',
+  devtool: 'cheap-module-source-map',
 
   output: {
     filename: 'static/js/[name].[hash].js'
